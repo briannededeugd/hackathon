99a922dd2a842a6a53dccafb6ad4602e521718aa
lainecedes <elaine.parieh@gmail.com>
2024-03-25 16:36:03 +0100

    ADD: Navigation.svelte component

---
+++ cssdayta/src/components/Navigation.svelte
@@ -0,0 +1,54 @@
+<!-- Script for navigation bar -->
+<script>
+
+</script>
+
+    <nav>
+      <a href="#" aria-label="CSS Day Logo">
+      <h1>CSS Day</h1>
+      <img src="https://cssday.nl/_img/cssday-logo.svg">
+        
+        <ul>
+          <li><a href="#">2024</a></li>
+          <li><a href="#">2023</a></li>
+          <li><a href="#">2022</a></li>
+          <li><a href="#">2019</a></li>
+          <li><a href="#">2018</a></li>
+          <li><a href="#">2017</a></li>
+          <li><a href="#">2016</a></li>
+          <li><a href="#">2015</a></li>
+          <li><a href="#">2014</a></li>
+          <li><a href="#">2013</a></li>
+        </ul>
+    </nav>
+
+<!-- Styling navigation bar -->
+<style>
+
+h1 {
+  display: none;
+}
+
+
+nav {
+/*   display: flex; */
+}
+
+a img {
+  width: 25%;
+}
+
+nav ul {
+  display: flex;
+  list-style: none;
+  justify-content: space-evenly;
+  padding: 0;
+  margin: 0;
+}
+
+nav ul li a {
+  text-decoration: none; 
+  color: inherit;
+}
+
+</style>
